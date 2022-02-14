1f40f4888910f200913231d85e48fc0f7cdfdc6b
Anil Beesetti <anilbeesetti10@gmail.com>
2022-02-14 09:01:31 +0530

    added scripts

--- .config/bin/launch_polybar.sh
+++ .config/bin/launch_polybar.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-if [[ ! `pidof polybar`  ]]; then
-    polybar -q main -c ~/.config/polybar/config &
-else
-    polybar-msg cmd restart
-fi
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Lauch the bar
+
+polybar -q main -c $HOME/.config/polybar/config.ini &
